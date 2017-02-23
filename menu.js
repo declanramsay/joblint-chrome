8fa930121a959143cfce93c188143d4d7c23c031
Declan Ramsay <declandr@gmail.com>
2017-02-24 01:08:44 +1300

    display count in popup

--- menu.js
+++ menu.js
@@ -5,6 +5,7 @@ function lint(text) {
   var h = 220;
   var left = (screen.width/2)-(w/2);
   var top = (screen.height/2)-(h/2);
+  
   chrome.windows.create({'url': 'results.html', 'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top}, function(window) {
     chrome.storage.sync.set({'results': results}, function() {
       console.log('Results stored!');
@@ -13,9 +14,9 @@ function lint(text) {
 }
 
 chrome.contextMenus.create({
-    id: "joblint",
-    title: "Check %s with joblint",
-    contexts:["selection"],
+  id: "joblint",
+  title: "Check %s with joblint",
+  contexts:["selection"],
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
--- results.js
+++ results.js
@@ -1,21 +1,30 @@
 chrome.storage.onChanged.addListener(function(changes, areaName) {
   var results = chrome.storage.sync.get('results', function(items) {
-    console.log('onChanged found the results');
-    console.log(items);
-
-    parseResults(items);
+    parseResults(items.results);
   });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
   var results = chrome.storage.sync.get('results', function(items) {
-    console.log('document Add event Listener found the results');
-    console.log(items);
-
-    parseResults(items);
+    parseResults(items.results);
   });
 });
 
 function parseResults(results) {
+  var counts = Object.keys(results.counts);
 
+  var countNode = document.querySelector('.count');
+
+  if(!counts.length) {
+    countNode.innerHTML = '<h1>No warnings!</h1>'
+    return;
+  }
+
+  var countsArr = counts.map(function(k) {
+    return k + ': ' + results.counts[k];
+  });
+
+  var countsString = countsArr.join('<br>');
+  countNode.innerHTML = '<h1>Count:</h1>' + countsString;
+  return;
 };
